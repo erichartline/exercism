f4323f36d2610a3467da1df1d5f7bd43e4431073
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-25 18:29:46 -0600

    feat: add go hello-world solution

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting displays a simple hello world message
+package greeting
+
+// HelloWorld returns a basic "Hello, World!" string
+func HelloWorld() string {
+	return "Hello, World!"
+}
