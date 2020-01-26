81962387477b8143705bfd5500526104eb844e07
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-26 10:21:41 -0600

    feat: add js resistor-color solution

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+const colorCode = color => COLORS.findIndex(item => item === color);
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
+
+export { colorCode, COLORS };
