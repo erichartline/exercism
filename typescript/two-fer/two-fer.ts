f77fd1cee3675fc3dd3f0e73e138ac68851ce53a
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-25 22:44:50 -0600

    feat: add two-fer ts solution

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,10 @@
+class TwoFer {
+  public static twoFer(name?: string): string {
+    if (!name) {
+      name = "you";
+    }
+    return `One for ${name}, one for me.`;
+  }
+}
+
+export default TwoFer;
